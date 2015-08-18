c5602bfb4720d6150ba2130384b79865bf64168b
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-08-18 16:48:23 -0700

    Run spec and binary within banzai

--- test_domino_program.py
+++ test_domino_program.py
@@ -3,6 +3,7 @@
 import sys
 import subprocess
 source_file = sys.argv[1]
+random_seed = sys.argv[2]
 
 # Get all renames from SSA
 sp = subprocess.Popen(["domino", source_file, "if_converter,strength_reducer,expr_flattener,expr_propagater,stateful_flanks,ssa"], stdout = subprocess.PIPE, stderr=subprocess.PIPE)
@@ -30,6 +31,19 @@ for field in fields:
 # Print out mapping
 print spec_to_impl_mapping
 
-# Compile impl.so and spec.so
-sp = subprocess.Popen(["domino", source_file, "if_converter,strength_reducer,expr_flattener,expr_propagater,stateful_flanks,ssa,banzai_binary"], stdout = open("./impl.so", "w"), stderr = open("/dev/null", "w"))
+# Compile spec.so and impl.so
 sp = subprocess.Popen(["domino", source_file, "banzai_binary"], stdout = open("./spec.so", "w"), stderr = open("/dev/null", "w"))
+sp.communicate()
+
+sp = subprocess.Popen(["domino", source_file, "if_converter,strength_reducer,expr_flattener,expr_propagater,stateful_flanks,ssa,banzai_binary"], stdout = open("./impl.so", "w"), stderr = open("/dev/null", "w"))
+sp.communicate()
+
+# Run both spec.so and impl.so on banzai
+sp = subprocess.Popen(["banzai", "./spec.so", random_seed, ",".join(fields)], stderr = subprocess.PIPE, stdout = open("/dev/null", "w"));
+out, err = sp.communicate()
+
+print err
+
+sp = subprocess.Popen(["banzai", "./impl.so", random_seed, ",".join(fields)], stderr = subprocess.PIPE, stdout = open("/dev/null", "w"));
+out, err = sp.communicate()
+print err
