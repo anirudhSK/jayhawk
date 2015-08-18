68efa13b70adcf1631117ccd7c1ceaaf8b16b7bf
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-08-18 16:09:31 -0700

    More work towards automated testing
    Build up data structure showing which fields in the spec & impl correspond

--- test_domino_program.py
+++ test_domino_program.py
@@ -2,13 +2,30 @@
 
 import sys
 import subprocess
-
 source_file = sys.argv[1]
+
+# Get all renames from SSA
 sp = subprocess.Popen(["domino", source_file, "if_converter,strength_reducer,expr_flattener,expr_propagater,stateful_flanks,ssa"], stdout = subprocess.PIPE, stderr=subprocess.PIPE)
 out, err = sp.communicate()
+renames = err.splitlines()
+rename_dict = dict()
+for rename in renames :
+  [orig, renamed] = rename.split()
+  rename_dict[orig] = renamed
 
-print "standard output "
-print out
+# Get all original fields
+sp = subprocess.Popen(["domino", source_file, "gen_pkt_fields"], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+out, err = sp.communicate()
+fields = out.splitlines()
 
-print "standard error "
-print err
+# Match up fields
+# from spec to implementation
+spec_to_impl_mapping = dict()
+for field in fields:
+  if field in rename_dict:
+    spec_to_impl_mapping[field] = rename_dict[field]
+  else:
+    spec_to_impl_mapping[field] = field
+
+# Print out mapping
+print spec_to_impl_mapping
