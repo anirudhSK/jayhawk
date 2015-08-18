197385f38f040521cb4222533f7789d127cecb01
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-08-18 16:52:44 -0700

    Pass csv of output fields

--- test_domino_program.py
+++ test_domino_program.py
@@ -31,6 +31,11 @@ for field in fields:
 # Print out mapping
 print spec_to_impl_mapping
 
+# Generate output fields in impl
+output_fields_in_impl = []
+for field in spec_to_impl_mapping:
+  output_fields_in_impl += [spec_to_impl_mapping[field]];
+
 # Compile spec.so and impl.so
 sp = subprocess.Popen(["domino", source_file, "banzai_binary"], stdout = open("./spec.so", "w"), stderr = open("/dev/null", "w"))
 sp.communicate()
@@ -39,11 +44,11 @@ sp = subprocess.Popen(["domino", source_file, "if_converter,strength_reducer,exp
 sp.communicate()
 
 # Run both spec.so and impl.so on banzai
-sp = subprocess.Popen(["banzai", "./spec.so", random_seed, ",".join(fields)], stderr = subprocess.PIPE, stdout = open("/dev/null", "w"));
+sp = subprocess.Popen(["banzai", "./spec.so", random_seed, ",".join(fields), ",".join(fields)], stderr = subprocess.PIPE, stdout = open("/dev/null", "w"));
 out, err = sp.communicate()
 
 print err
 
-sp = subprocess.Popen(["banzai", "./impl.so", random_seed, ",".join(fields)], stderr = subprocess.PIPE, stdout = open("/dev/null", "w"));
+sp = subprocess.Popen(["banzai", "./impl.so", random_seed, ",".join(fields), ",".join(output_fields_in_impl)], stderr = subprocess.PIPE, stdout = open("/dev/null", "w"));
 out, err = sp.communicate()
 print err
